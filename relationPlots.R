f387eb491ee7ad6e6bf1b610e7795cfa60d2d42e
davidtryan <david.ryan@datamininglab.com>
2014-12-09 14:51:43 -0500

    Plots formatted for final output appropriately - checking error messages still

--- relationPlots.R
+++ relationPlots.R
@@ -27,20 +27,21 @@ relationPlots <- function(df, df_T, file='output.pdf', wordy=F){
 #       screen(1)
       close.screen(all = TRUE)    # exit split-screen mode
 #       plot(0,xaxt='n',yaxt='n',bty='n',pch='',ylab='',xlab='')
+#       par(oma=c(0,0,10,0))
       split.screen(c(2,1))
-      plotScat(df[,i], df_T_data, vn=names(df)[i], tn=df_T)
-      plotNum(df[,i], vn=names(df)[i], singPlot=0)
+      plotScat(df[,i], df_T_data, vn=names(df)[i], tn=df_T, singPlot=0)
+      plotNum(df[,i], vn=names(df)[i], singPlot=0, tn=df_T)
       
     } else if ((class(df[,i]) %in% c('numeric', 'integer')) & length(unique(df[,i]))<=15) { 
 #       screen(1)
       close.screen(all = TRUE)    # exit split-screen mode
 #       plot(0,xaxt='n',yaxt='n',bty='n',pch='',ylab='',xlab='')
       split.screen(c(2,1))
-      plotScat(df[,i], df_T_data, vn=names(df)[i], tn=df_T)
-      plotChar(df[,i], vn=names(df)[i], singPlot=0)
+      plotScat(df[,i], df_T_data, vn=names(df)[i], tn=df_T, singPlot=0)
+      plotChar(df[,i], vn=names(df)[i], singPlot=0, tn=df_T)
 #       close.screen(all = TRUE)    # exit split-screen mode
     } else { 
-      plotChar(df[,i], vn=names(df)[i], singPlot=1)
+      plotChar(df[,i], vn=names(df)[i], singPlot=1, tn=df_T)
     }
   }
   graphics.off()
@@ -53,25 +54,28 @@ relationPlots <- function(df, df_T, file='output.pdf', wordy=F){
 ## Helper functions for plotdf (not included in Rsenal package)
 ##################################################################
 
-plotScat <- function(x, y, vn, tn) {
+plotScat <- function(x, y, vn, tn, singPlot) {
 #   par(mfrow = c(1,1), mar=c(5,4,4,2), oma=c(0,0,0,0))
   if (dim(table(x))<=4 & dim(table(y))==2) {
     split.screen(c(2,2), screen=1)
+#     screen(1)
+#     text(x=0.015, y=0.8, label='type:', pos=4, font=2, cex=0.4)
 #     par(mfrow = c(1,1), mar=c(5,4,4,2), oma=c(0,0,0,0))
     par(mar=c(5,4,4,2))
 #     par(mfrow=c(2,2),mar=c(5,4,2,2), oma=c(0,0,3,0))
     screen(3)
-    par(mar=c(3,2,2,1))
-    plot(x, y, ylab=tn)
+#     par(mar=c(4,4,1,1))
+    par(mar=c(2,2,2,1))
+    plot(x, y, xlab=vn, ylab=tn)
     screen(4)
-    par(mar=c(3,2,2,1))
+    par(mar=c(2,2,2,1))
     jitterScat(x,y,vn,tn)
     grid()
     screen(5)
-    par(mar=c(3,2,2,1))
+    par(mar=c(2,2,2,1))
     stackedBar(x,y,vn,tn)
     screen(6)
-    par(mar=c(3,2,2,1))
+    par(mar=c(2,2,2,1))
     sideBar(x,y,vn,tn)
 #     if (length(unique(df[,i]))<=15) {
 #       plotChar(df[,i], vn=names(df)[i])
@@ -94,12 +98,16 @@ plotScat <- function(x, y, vn, tn) {
 #     } else {
 #       plotNum(df[,i], vn=names(df)[i])
 #     }
-    mtext(sprintf("Plots of %s vs %s", vn, tn), outer=TRUE, cex=1.5)
+#     mtext(sprintf("Plots of %s vs %s", vn, tn), outer=TRUE, cex=1.5)
   } else {
     screen(1)
     par(mar=c(4,4,4,1))
 #     par(mfrow=c(1,1))
-    plot(x,y,xlab=vn,ylab=tn, main=sprintf("Scatterplot of %s vs %s", vn, tn))
+    if (singPlot==0) {
+      plot(x,y,xlab=vn,ylab=tn)
+    } else {
+      plot(x,y,xlab=vn,ylab=tn, main=sprintf("Scatterplot of %s vs %s"))
+    }
 #     if (length(unique(df[,i]))<=15) {
 #       plotChar(df[,i], vn=names(df)[i])
 #     } else {
@@ -114,7 +122,7 @@ stackedBar <- function(x, y, vn, tn) {
 
 sideBar <- function(x,y,vn,tn) {
   barplot(table(y,x), beside=T, col=c("darkblue","red"), xlab=vn, ylab=tn)
-  legend("topright", rownames(table(y, x)))
+  legend("topright", rownames(table(y, x)), pch=15, col=c("darkblue","red"), cex=0.75, bty='n')
 }
 
 jitterScat <- function(x,y,vn,tn) {
@@ -124,12 +132,20 @@ jitterScat <- function(x,y,vn,tn) {
 
 
 
-plotNum <- function(x, singPlot, ...) {
+plotNum <- function(x, singPlot, tn, ...) {
   if (singPlot==0) {
     screen(2)
     ptitle <- ifelse(is.null(list(...)$vn), '', list(...)$vn)
     p2<-hist(x, main=ptitle, breaks=30, xlab=ptitle, col='grey')
     rug(x, col='red')
+    
+    #######################
+    op <- par(fig = c(0.55,0.95,0.125,0.525), new = TRUE)
+    par(mfrow=c(1,1),mar=c(5,4,4,2))
+    plot(0,xaxt='n',yaxt='n',bty='n',pch='',ylab='',xlab='')
+    mtext(sprintf("Plots of %s vs %s", ptitle, tn), outer=F, cex=1.0, line=3, side=3, font=2)
+    par(op)
+    ########################
     
     #######################
     op <- par(fig = c(0.55,0.95,0.125,0.525), new = TRUE)
@@ -155,7 +171,6 @@ plotNum <- function(x, singPlot, ...) {
     box(lwd=1)
     par(op)
     par(mfrow=c(1,1),mar=c(5,4,4,2))
-    ########################
     
   } else {
     par(mfrow=c(3, 2))
@@ -196,40 +211,41 @@ plotNum <- function(x, singPlot, ...) {
 ## plotNum(rnorm(1000)^2, vn='ssds')
 
 
-plotChar <- function(x, singPlot, ...) {
+plotChar <- function(x, singPlot, tn, ...) {
   if (singPlot==0) { 
     screen(2) 
-    par(mar=c(5,4,4,1))
-    
-    
-#     par(mfrow=c(3, 2))
-#     layout(matrix(c(1,1,2,2,2,2), 3, 2, byrow=T), widths=c(1,1), heights=c(1,1,1))
     
     ptitle <- ifelse(is.null(list(...)$vn), '', list(...)$vn)
     tab <- sort(table(x), decreasing=T)[1:min(length(unique(x)),50)]
     tabmiss <- round(sum(tab, na.rm=T)/length(x)*100, 1)
 
-#     par(mar=c(10,4.1,4.1,2.1))
-    p2 <- barplot(tab, las=2, cex.names=0.6, col='dodgerblue',
+    p2 <- barplot(tab, las=2, cex.names=0.6, col='dodgerblue', xlab = ptitle
                   main=paste(ptitle, ': ', tabmiss, '% of data shown', sep=''))
-#     par(mar=c(5.1,4.1,4.1,2.1))
     
     #######################
-    op <- par(fig = c(0.6,0.9,0.225,0.525), new = TRUE)
+    op <- par(fig = c(0.55,0.95,0.125,0.525), new = TRUE)
+    par(mfrow=c(1,1),mar=c(5,4,4,2))
+    plot(0,xaxt='n',yaxt='n',bty='n',pch='',ylab='',xlab='')
+    mtext(sprintf("Plots of %s vs %s", ptitle, tn), outer=F, cex=1.0, line=3, side=3, font=2)
+    par(op)
+    ########################
+    
+    #######################
+    op <- par(fig = c(0.6,0.95,0.225,0.525), new = TRUE)
 
     p1 <- plot(0:1, 0:1, col='white', yaxt='n', ylab = '', xaxt='n', xlab='')
-    text(x=0.01, y=0.8, label='type:', pos=4, font=2, cex=0.4)
-    text(x=0.01, y=0.5, label='# of unique values:', pos=4, font=2, cex=0.4)
-    text(x=0.01, y=0.4, label='% NA:', pos=4, font=2, cex=0.4)
+    text(x=0, y=0.8, label='type:', pos=4, font=2, cex=0.4)
+    text(x=0, y=0.5, label='# of unique values:', pos=4, font=2, cex=0.4)
+    text(x=0, y=0.2, label='% NA:', pos=4, font=2, cex=0.4)
     
-    text(x=0.3, y=0.8, label=class(x), pos=4, cex=0.4)
-    text(x=0.3, y=0.5, label=length(unique(x)), pos=4, cex=0.4)
-    text(x=0.3, y=0.4, label=paste0(round(sum(is.na(x))/length(x)*100, 4), '%'), pos=4, cex=0.4)
+    text(x=0.65, y=0.8, label=class(x), pos=4, cex=0.4)
+    text(x=0.65, y=0.5, label=length(unique(x)), pos=4, cex=0.4)
+    text(x=0.65, y=0.2, label=paste0(round(sum(is.na(x))/length(x)*100, 4), '%'), pos=4, cex=0.4)
     
     box(lwd=1)
     par(op)
     par(mfrow=c(1,1),mar=c(5,4,4,2))
-    
+  
   } else {
     par(mfrow=c(3, 2))
     layout(matrix(c(1,1,2,2,2,2), 3, 2, byrow=T), widths=c(1,1), heights=c(1,1,1))  
