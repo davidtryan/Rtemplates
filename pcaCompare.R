0b8a411424e25c30b57dca02a55d36cdf59d440c
davidtryan <david.ryan@datamininglab.com>
2015-01-07 13:22:53 -0500

    Final first draft of pcaCompare - need to check over for completeness

--- pcaCompare.R
+++ pcaCompare.R
@@ -4,6 +4,7 @@
 # http://www.statistik.tuwien.ac.at/public/filz/students/seminar/ws1011/hoffmann_ausarbeitung.pdf
 # http://factominer.free.fr/classical-methods/principal-components-analysis.html
 # http://www.inside-r.org/packages/cran/FactoMineR/docs/plot.PCA
+# http://pbil.univ-lyon1.fr/ade4/ade4-html/olympic.html
 
 ###################################################################
 ## Installing and initializing
@@ -37,7 +38,7 @@ PCA.olympic <- PCA(olympic$tab, scale.unit=T, ncp=5, graph=T)
 
 ###################################################################
 ## compare loadings
-#princomp pca.olympic$loadings
+#FactoMineR PCA.olympic$loadings
 PCA.olympic.loadings <- sweep(PCA.olympic$var$coord, 2, sqrt(PCA.olympic$eig[1:5,1]), FUN="/")
 
 #FactoMineR PCA.olympic.loadings
@@ -273,6 +274,16 @@ insigVars
 
 #Qualitative supplementary variables
 #....
+#To see whether the categories of the supplementary variable are significantly different from each other, we can draw confidence ellipses around them.
+concat <- cbind.data.frame(decathlon[,13], res.PCA$ind$coord)
+ellipse.coord <- coord.ellipse(concat, bary=T)
+plot.PCA(res.PCA, habillage=13, ellipse=ellipse.coord, cex=0.8, axes=c(1,3))
+#When looking at the points representing "Decastar" and "Olympic Games", we notice that this last one has higher coordinates on the 
+#first axis than the first one. This shows an evolution in the performances of the athletes. All the athletes who participated to the two 
+#competitions have then slightly better results for the Olympic Games. 
+#However, there is no difference between the points "Decastar" and "Olympic Games" for the second axis. This means that the athletes have 
+#improved their performance but did not change profile (except for Zsivoczky who went from slow and strong during the Decastar to fast and 
+#weak during the Olympic Games).
 
 #Color individuals according to the categories' center of gravity
 # plot.PCA(res.pca, axes=c(1, 2), choix="ind", habillage=13)
@@ -307,7 +318,6 @@ estim_ncp(olympic_p, ncp.min=0, ncp.max=NULL, scale=TRUE, method="GCV")
 
 
 ###################################################################
-
 #Selecting individuals with specific features to plot
 #CAN ONLY BE DONE WITH FACTOMINER
 #Plot individuals who have a cos2>0.7
@@ -324,70 +334,83 @@ plot.PCA(res.PCA, axes=c(1, 2), choix="ind", habillage=13, cex=0.8, shadow=T, se
 
 
 ###################################################################
+#Graphing variables
+#FactoMineR
+plot(res.PCA, choix='var', shadow=T)
+plot(res.PCA, choix='var', shadow=T, select="contrib 5")
+plot(res.PCA, choix='var', shadow=T, select="cos2 5")
+          
+#princomp
+biplot(res.pca, main='primcomp Results', col=c('white', 'black'))
+abline(h=0, lty=2)
+abline(v=0, lty=2)
+###################################################################
 
-          #Graphing variables
-          plot(res.PCA, choix='var', shadow=T)
-          plot(res.PCA, choix='var', shadow=T, select="contrib 5")
-          plot(res.PCA, choix='var', shadow=T, select="cos2 5")
+
+###################################################################
+## Ranking variable contribution for combined first two PCs
+#FactoMineR
+dimsum <- res.PCA$var$contrib[,1]          
+for (i in 1:dim(res.PCA$var$contrib)[1]) {
+  dimsum[i]<- sum(res.PCA$var$contrib[i,1:2])
+}
+sort(dimsum, decreasing=T)
+
+for (i in 1:dim(res.PCA$var$cos2)[1]) {
+  dimsum[i]<- sum(res.PCA$var$cos2[i,1:2])
+}
+sort(dimsum, decreasing=T)
+
+#princomp
+impList <- res.pca$loadings[,1:2]
+impList <- sort(abs(impList[,1])+abs(impList[,2]))
+maxInd <- which(impList==max(abs(impList)))
+maxIndList <- c(maxInd)
+swp <- 0
+while (swp==0 && length(maxIndList)<4) {
+  catImpList <- impList[-c(maxIndList)]
+  maxInd_t <- which(abs(impList)==max(abs(catImpList)))
+  if (maxInd_t!=(maxInd-1)) {
+    swp=1
+  }
+  maxInd <- maxInd_t
+  maxIndList <- c(maxIndList, maxInd_t)
+}
+impNums <- impList[maxIndList]
+names(impNums)
+###################################################################
+
+
+###################################################################
+## Combined summary plots
+#FactoMineR
+#http://pbil.univ-lyon1.fr/ade4/ade4-html/olympic.html
+par(mfrow=c(2,2))
+pc.bp <- barplot(res.PCA$eig[,1])
+lines(x=pc.bp, y=res.PCA$eig[,1])
+points(x=pc.bp, y=res.PCA$eig[,1])
+
+s.corcircle(res.PCA$var)
+
+plot(decathlon$Points, res.PCA$ind$coord[,1])
+abline(lm(res.PCA$ind$coord[,1]~decathlon$Points))
+
+s.label(res.PCA$ind$coord, clab=0.5)
+s.arrow(2*res.PCA$var$coord, add.p=T)
           
-          #Ranking variable contribution
-          for (i in 1:dim(res.pca$var$contrib)[1]) {
-            dimsum[i]<- sum(res.pca$var$contrib[i,1:2])
-          }
-          sort(dimsum, decreasing=T)
-          
-          for (i in 1:dim(res.pca$var$cos2)[1]) {
-            dimsum[i]<- sum(res.pca$var$cos2[i,1:2])
-          }
-          sort(dimsum, decreasing=T)
-          
-          #When looking at the points representing "Decastar" and "Olympic Games", we notice that this last one has higher coordinates on the 
-          #first axis than the first one. This shows an evolution in the performances of the athletes. All the athletes who participated to the two 
-          #competitions have then slightly better results for the Olympic Games. 
-          #However, there is no difference between the points "Decastar" and "Olympic Games" for the second axis. This means that the athletes have 
-          #improved their performance but did not change profile (except for Zsivoczky who went from slow and strong during the Decastar to fast and 
-          #weak during the Olympic Games).
-          
-          #We can see that the points which represent the same individual are in the same direction. For example, Sebrle got good results in both 
-          #competition but the point which represents his performance during the O.G. is more extreme. Sebrle got more points during the O.G. than during the Decastar.
-          
-          
-          #These tables give the correlation coefficient and the p-value of the variables which are significantly correlated to the principal dimensions. 
-          #Both active and supplementary variables whose p-value is smaller than 0.05 appear.
-          dimdesc(res.pca, axes=c(1,2))
-          #The tables of the description of the two principal axes show that the variables "Points" and "Long.jump" are the most correlated to the first dimension 
-          #and "Discus" is the most correlated to the second one. This confirms our first interpretation that atheletes who participate in O.G. are better than those
-          #who participate in Decastar
-          
-          
-          #To see whether the categories of the supplementary variable are significantly different from each other, we can draw confidence ellipses around them.
-          concat <- cbind.data.frame(decathlon[,13], res.pca$ind$coord)
-          ellipse.coord <- coord.ellipse(concat, bary=T)
-          plot.PCA(res.pca, habillage=13, ellipse=ellipse.coord, cex=0.8, axes=c(1,3))
-          
-          
-          
-          
-          
-          
-          
-          
-          
-          #http://pbil.univ-lyon1.fr/ade4/ade4-html/olympic.html
-          par(mfrow=c(2,2))
-          pc.bp <- barplot(res.pca$eig[,1])
-          lines(x=pc.bp, y=res.pca$eig[,1])
-          points(x=pc.bp, y=res.pca$eig[,1])
-          
-          plot(res.pca, choix='var', shadow=T)
-          
-          #library(ade4)
-          # plot(decathlon$Points, res.pca$dist)
-          plot(decathlon$Points, res.pca$ind$coord[,1])
-          abline(lm(res.pca$ind$coord[,1]~decathlon$Points))
-          
-          s.label(res.pca$ind$coord, clab=0.5)
-          s.arrow(2*res.pca$var$coord, add.p=T)
-          
-          par(mfrow=c(1,1))
-          plot(res.pca$eig[,1], decathlon$Points, xlab="Dim1", pch=23, bg='red', cex=2)
+#princomp
+par(mfrow=c(2,2))
+pc.bp <- plot(res.pca)
+
+pca.var.coord <- res.pca$loadings[,1:5]
+pca.var.coord[,c(1,3,4,5)] <- -pca.var.coord[,c(1,3,4,5)]
+s.corcircle(pca.var.coord)
+
+plot(decathlon$Points, -res.pca$scores[,1])
+abline(lm(-res.pca$scores[,1]~decathlon$Points))
+
+pca.coord <- -res.pca$scores[,1:5]
+pca.coord[,1:2] <- -s.label[,1:2]
+s.label(pca.coord, clab=0.5)
+s.arrow(2*pca.var.coord, add.p=T)
+
